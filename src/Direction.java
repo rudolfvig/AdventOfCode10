05d6fadb196961f2c34219cd3fd6749651fbd8ec
vigRudolf <vrudi68@gmail.com>
2024-07-04 09:52:56 +0200

    Direction class

--- src/Direction.java
+++ src/Direction.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Direction {
+public class Direction {
+
+    private final Integer directionX;
+    private final Integer directionY;
+
+    public Direction(int x, int y) {
+        this.directionX = x;
+        this.directionY = y;
+    }
+
+    public Integer getDirectionX() {
+        return directionX;
+    }
+
+    public Integer getDirectionY() {
+        return directionY;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        HashMap<Character, Integer[]> mapTiles = new HashMap<>();
+        Direction up = new Direction(1,0);
+        Direction down = new Direction(-1,0);
+        Direction right = new Direction(0,1);
+        Direction left = new Direction(-1,0);
+
+
+
     }
 }
